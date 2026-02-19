a45fb4558166e7b32d08c8282fee5baf16be4e7c
RushiLukka <rushi.lukka.14@gmail.com>
2026-02-19 09:33:13 +0530

    feat: Initialize backend with an Express server, TypeScript configuration, and essential development files.

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("Backend initialized with TypeScript");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const db_1 = __importDefault(require("./config/db"));
+// Load env vars
+dotenv_1.default.config();
+// Connect to database
+(0, db_1.default)();
+const app = (0, express_1.default)();
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
+const caseRoutes_1 = __importDefault(require("./routes/caseRoutes"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_1 = __importDefault(require("./config/swagger"));
+// Middleware
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}));
+app.use((0, helmet_1.default)());
+app.use((0, morgan_1.default)('dev'));
+// Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+app.use('/api/docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_1.default));
+app.use('/api/auth', authRoutes_1.default);
+app.use('/api/cases', caseRoutes_1.default);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
